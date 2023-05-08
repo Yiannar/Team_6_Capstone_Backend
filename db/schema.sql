9510c48f0d164e7c4d7105984eeb44754ebecdc7
Yiannar <yiannarichey@pursuit.org>
2023-05-08 10:43:01 -0400

    starting schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,11 @@
--- DROP DATABASE IF EXISTS template_test_dev;
--- CREATE DATABASE template_test_dev;
+DROP DATABASE IF EXISTS template_test_dev;
+CREATE DATABASE template_test_dev;
 
--- \c template_test_dev;
+\c template_test_dev;
 
--- DROP TABLE IF EXISTS test;
+DROP TABLE IF EXISTS test;
 
--- CREATE TABLE test (
---     id SERIAL PRIMARY KEY, 
---     name TEXT
--- );
+CREATE TABLE test (
+    id SERIAL PRIMARY KEY, 
+    name TEXT
+);
