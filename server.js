2c18400f7b78c99b2f7e03323c897c2067ba14db
Yiannar <yiannarichey@pursuit.org>
2023-05-09 15:39:51 -0400

    added some socket info

--- server.js
+++ server.js
@@ -1,37 +1,41 @@
 // DEPENDENCIES
-const { Socket } = require("socket.io");
 const app = require("./app.js");
-
-
 
 // CONFIGURATION
 require("dotenv").config();
 const PORT = process.env.PORT;
 
-// LISTEN
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+const  {socket}  = require("socket.io");
+const {instrument} = require ('@socket.io/admin-ui')
 
 
 
-const io = require('socket.io')(3003, {
+const io = require('socket.io')(3004, {
     cors:{
-        origin: ['http://localhost:3003']
+        origin: ['https://kyrun.netlify.app/', 'https://admin.socket.io/']
     }
 })
 
 io.on('connection', socket =>{
     console.log(socket.id)
-    socket.on('send-message', message => {
+    socket.on('send-message', (message, room) => {
         if(room === ''){ 
             socket.broadcast.emit('recieve-message', message)
         } else {
             socket.to(room).emit('recieve-message', message)
         }
     })
+    socket.on('join-room', (room, cb) =>{
+        socket.join(room)
+        cb(`Joined ${room}`)
+    })
 })
-socket.on('join-room', (room,cb) =>{
-    socket.join(room)
-    cb(`Joined ${room}`)
-})
+
+instrument(io, {auth: false})
+
+// LISTEN
+io.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
+  
+  
