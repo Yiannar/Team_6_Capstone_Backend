13e577244ff89b16c3ed9f6a053054b4ceb906c9
Yiannar <yiannarichey@pursuit.org>
2023-05-16 12:15:25 -0400

    tables are working

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,15 @@ CREATE DATABASE user_profile;
 -- make table for authentication
 -- if micorsoft authentication  then verification is verified
 
+DROP TABLE IF EXISTS groups;
+
+CREATE TABLE groups (
+   id SERIAL PRIMARY KEY,
+   title TEXT NOT NULL,
+   about TEXT,
+   img TEXT NOT NULL
+);
+
 DROP TABLE IF EXISTS profile;
 
 CREATE TABLE profile (
@@ -19,17 +28,29 @@ CREATE TABLE profile (
   zipCode INT,
   pace INT,
   gender TEXT,
-  verified BOOLEAN DEFAULT false
+  verified BOOLEAN DEFAULT false,
+  image TEXT DEFAULT 'image not found',
+  groups_id INTEGER REFERENCES groups (id)
 );
 
-DROP TABLE IF EXISTS bulletin (
-  bulletin_id INT PRIMARY KEY,
-  title VARCHAR(255) NOT NULL,
-  message TEXT NOT NULL,
-  author TEXT NOT NULL,
-  date TEXT NOT NULL
-  FOREIGN KEY(author) REFERENCES profile (id)
-  -- group TEXT NOT NULL,
-  FOREIGN KEY(group) REFERENCES group(group_id)
-)
 
+
+DROP TABLE IF EXISTS posts;
+
+CREATE TABLE posts(
+    id SERIAL PRIMARY KEY,
+    post TEXT,
+    date TEXT NOT NULL,
+    author_id INTEGER REFERENCES profile(id),
+   groups_id INTEGER REFERENCES groups(id)
+);
+
+DROP TABLE IF EXISTS replies;
+
+CREATE TABLE replies(
+    id SERIAL PRIMARY KEY,
+    reply TEXT,
+    date TEXT NOT NULL,
+    post_id INTEGER REFERENCES posts(id),
+    author_id INTEGER REFERENCES profile(id)
+);
--- db/seed.sql
+++ db/seed.sql
@@ -1,28 +1,29 @@
 \c user_profile;
 
-INSERT INTO profile (first_name,last_name, email, password, age, zipCode, pace, gender, verified) VALUES
-('Jane', 'Doe', 'Janedoe@gmail.com', 'apple', 23, '11206', '4', 'Female', true ),
-('John', 'Doe', 'Johndoe@gmail.com', 'orange', 27, '11106', '2', 'Male', true );
+
+INSERT INTO groups (title, about, img) VALUES
+('Early Morning Run','For the early birds lets meet up at 5am!','https://t3.ftcdn.net/jpg/03/52/54/64/240_F_352546427_SlRYT8y5pnn1NM2ilDM4heFA3oNnIwya.jpg'),
+('Brooklyn Joggers','For the Brooklynites that want to run around the borough','https://images.pexels.com/photos/944690/pexels-photo-944690.jpeg?cs=srgb&dl=pexels-chris-molloy-944690.jpg&fm=jpg' );
 
 
 
 
-\c login;
-
-INSERT INTO login(email)VALUES
-('Janedoe@gmail.com')
-('Johndoe@gmail.com')
+INSERT INTO profile (first_name,last_name, email, password, age, zipCode, pace, gender,verified,image, groups_id) VALUES
+('Jane', 'Doe', 'Janedoe@gmail.com', 'apple', 23, '11207', '4', 'Female',true,'https://d39l2hkdp2esp1.cloudfront.net/img/photo/206402/206402_00_2x.jpg', 1),
+('John', 'Doe', 'Johndoe@gmail.com', 'orange', 27, '11106', '2', 'Male', true,'https://img.freepik.com/free-photo/side-view-man-jumping-outdoors_23-2149557835.jpg?w=2000', 2 );
 
 
-\c posts;
 
-INSERT INTO posts(post, date)
-('Hey I am available to meet today', 'March 15th,2023')
-('Hey I am not available to meet today', 'March 15th,2023')
 
-\c replies;
 
-INSERT INTO replies(reply, date)
-('Hey that sounds great cannot wait to meet up! ', 'March 15th,2023')
-('Hey oh no I really wanted to meet up with you', 'March 15th,2023')
+
+INSERT INTO posts(post, date, author_id, groups_id) VALUES
+('Hey I am available to meet today', 'March 15th,2023', 1, 1),
+('Hey I am not available to meet today', 'March 15th,2023', 2, 2);
+
+
+
+INSERT INTO replies(reply, date, post_id, author_id) VALUES
+('Hey that sounds great cannot wait to meet up! ', 'March 15th,2023', 1, 1),
+('Hey oh no I really wanted to meet up with you', 'March 15th,2023', 2 ,2 )
 
