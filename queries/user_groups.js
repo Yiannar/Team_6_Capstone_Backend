e98e50dd6284b293a3ede9088836b787ed9d57e2
Yiannar <yiannarichey@pursuit.org>
2023-05-19 13:50:47 -0400

    working dev branch exlcuding usergroups

--- queries/user_groups.js
+++ queries/user_groups.js
@@ -10,35 +10,37 @@ const getAllUserGroups = async () => {
   }
 };
 
-const getAllGroupsSingleUser = async (profileID) => {
+const getAllGroupsSingleUser = async (profile_id) => {
   try {
-    // const singleUserGroups = db.any('SELECT * FROM profile WHERE profile_id = $1', profileID);
-    const singleGroups = await db.any(
-      'SELECT groups.id, groups.title, groups.about FROM groups JOIN profile_groups ON groups.id = profile_groups.group_id JOIN users ON profile_groups.profile_id = profile.id WHERE profile.id = $1',
-      profileID
-    );
-    return singleGroups;
+    const singleUserGroups = db.any('SELECT * FROM profile WHERE profile_id = $1', profile_id);
+    // const singleGroups = await db.any(
+    //   'SELECT groups.id, groups.title, groups.about FROM groups JOIN profile_groups ON groups.id = profile_groups.group_id JOIN profile ON profile_groups.profile_id = profile.id WHERE profile.id = $1',
+    //   profile_id
+    // );
+    console.log('Hello There')
+    return singleUserGroups;
+    
   } catch (error) {
     return error;
   }
 };
 
-const joinAGroup = async (profileID, groupID) => {
+const joinAGroup = async (profile_id, group_id) => {
   try {
     const groupJoined = await db.one(
-      'INSERT INTO profile_groups(profileID, groupID) VALUES ($1, $2) RETURNING *',
-      [profileID, groupID]
+      'INSERT INTO profile_groups(profile_id, group_id) VALUES ($1, $2) RETURNING *',
+      [profile_id, group_id]
     );
     return groupJoined;
   } catch (error) {
     return error;
   }
 };
-const leaveAGroup = async (profileID, groupID) => {
+const leaveAGroup = async (profile_id, group_id) => {
   try {
     const leftGroup = await db.one(
-      'DELETE FROM profile_groups WHERE profileID = $1 AND groupID=$2 RETURNING *',
-    [profileID, groupID]
+      'DELETE FROM profile_groups WHERE profile_id = $1 AND group_id=$2 RETURNING *',
+    [profile_id, group_id]
     );
     return leftGroup;
   } catch (error) {
