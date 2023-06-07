12baf75ffe4c0b2f24ac53078db73410e28bbd01
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2023-06-07 13:31:13 -0400

    seed data for rating, spelling issue userGroups

--- controller/userGroupsController.js
+++ controller/userGroupsController.js
@@ -9,7 +9,6 @@ const {
   joinAGroup,
   leaveAGroup,
 } = require('../queries/user_groups');
-
 //  This gets all the groups the user belongs to
 usersGroups.get('/', async (req, res) => {
   const groups = await getAllUserGroups();
@@ -21,7 +20,6 @@ usersGroups.get('/', async (req, res) => {
       .json({ error: 'Unable to get all groups user is member of' });
   }
 });
-
 // this gets all the group the user belongs to at that particular id
 usersGroups.get('/:profile_id/', async (req, res) => {
   const { profile_id } = req.params;
@@ -30,30 +28,23 @@ usersGroups.get('/:profile_id/', async (req, res) => {
   if (!group.message) {
     res.status(200).json(group);
   } else {
+    res.status(400).json({ error: 'group Not found' });
+  }
+});
+// gets all profiles that belong to a groups_id
+usersGroups.get('/groups/:groups_id', async (req, res) => {
+  const { groups_id } = req.params;
+  const group = await getAllGroups(groups_id);
+  console.log('group', group);
+  if (!group.message) {
+    res.status(200).json(group);
+  } else {
     res.status(400).json({ error: ' group Not found' });
   }
 });
-
-// // This should allow the user to join a group at that groupID hopefully
-// usersGroups.post('/', async (req, res) => {
-// =======
-// // gets all profiles that belong to a groups_id
-// usersGroups.get('/groups/:groups_id', async (req, res) => {
-//   const { groups_id} = req.params;
-//   const group = await getAllGroups(groups_id);
-//   console.log('group', group);
-//   if (!group.message) {
-//     res.status(200).json(group);
-//   } else {
-//     res.status(400).json({ error: ' group Not found' });
-//   }
-// });
-
-
-
 // count to see how many users are in a group
 usersGroups.get('/:groups_id/profiles/count', async (req, res) => {
-  const { groups_id} = req.params;
+  const { groups_id } = req.params;
   const group = await countGroupMembers(groups_id);
   console.log('group', group);
   if (!group.message) {
@@ -62,8 +53,6 @@ usersGroups.get('/:groups_id/profiles/count', async (req, res) => {
     res.status(400).json({ error: ' group Not found' });
   }
 });
-
-
 // this gets all the group the user belongs to at that particular profile_id
 usersGroups.get('/:profile_id/:groups_id', async (req, res) => {
   const { profile_id, groups_id } = req.params;
@@ -79,10 +68,7 @@ usersGroups.get('/:profile_id/:groups_id', async (req, res) => {
     res.status(500).json({ error: 'Internal server error' });
   }
 });
-
-
-
-// This works now to join 
+// This works now to join
 usersGroups.post('/:profile_id/:groups_id', async (req, res) => {
   const { profile_id, groups_id } = req.params;
   try {
@@ -92,7 +78,6 @@ usersGroups.post('/:profile_id/:groups_id', async (req, res) => {
     res.status(500).json({ error: 'cannot join group' });
   }
 });
-
 // This should allow the user to leave a group at that profileID,groupID hopefully but needs work
 usersGroups.delete('/:profile_id/:groups_id', async (req, res) => {
   try {
@@ -103,5 +88,4 @@ usersGroups.delete('/:profile_id/:groups_id', async (req, res) => {
     res.status(500).json({ error: 'invalid request' });
   }
 });
-
 module.exports = usersGroups;
